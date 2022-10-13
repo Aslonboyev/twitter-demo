fb8567a69840b22fb7fc23559213ca8356aabe7a
temurofficial07 <temuraslonboyev@gmail.com>
2022-10-13 19:54:16 +0500

    ;

--- src/BlogApp.WebApi/Controllers/BlogPostsController.cs
+++ src/BlogApp.WebApi/Controllers/BlogPostsController.cs
@@ -24,11 +24,11 @@ namespace BlogApp.WebApi.Controllers
 
         [HttpGet("{userid}/blogposts"), Authorize(Roles = "User, Admin")]
         public async Task<IActionResult> GetAllByBlogPostAsync(long userid, [FromQuery] PaginationParams @params)
-            => Ok(await _postService.GetAllAsync(@params, p => p.UserId == userid && p.ItemState == Enums.ItemState.Active));
+            => Ok(await _postService.GetAllAsync(@params, p => p.UserId == userid));
 
         [HttpGet("{id}"), Authorize(Roles = "User, Admin")]
         public async Task<IActionResult> GetAsync(long id)
-            => Ok(await _postService.GetAsync(p => p.Id == id && p.ItemState == Enums.ItemState.Active));
+            => Ok(await _postService.GetAsync(p => p.Id == id));
 
         [HttpDelete("{userid}/blogposts"), Authorize(Roles = "User, Admin")]
         public async Task<IActionResult> DeleteRange(long userid)
@@ -36,7 +36,7 @@ namespace BlogApp.WebApi.Controllers
 
         [HttpDelete("{id}"), Authorize(Roles ="User, Admin")]
         public async Task<IActionResult> DeleteAsync(long id)
-            => Ok(await _postService.DeleteAsync(p => p.Id == id && p.ItemState == Enums.ItemState.Active )); 
+            => Ok(await _postService.DeleteAsync(p => p.Id == id)); 
 
         [HttpPost, Authorize(Roles = "User, Admin")]
         public async Task<IActionResult> CreateAsync([FromForm] BlogPostCreateViewModel blogPostCreateViewModel)
