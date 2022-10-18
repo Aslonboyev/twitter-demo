d59cc74c4632a72bb2a8da707af85fc66735d5e5
temurofficial07 <temuraslonboyev@gmail.com>
2022-10-18 23:16:25 +0500

    cc

--- src/BlogApp.WebApi/Migrations/20221018181558_A06.Designer.cs
+++ src/BlogApp.WebApi/Migrations/20221018181558_A06.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BlogApp.WebApi.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BlogApp.WebApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221018181558_A06")]
+    partial class A06
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
